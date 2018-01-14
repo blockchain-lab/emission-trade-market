7182f358acc2647ffd5e879f07c9b993f9da0ebd
huanggze <gzhuang2014@gmail.com>
2018-01-14 12:27:30 +0100

    regulator page

--- web/app/controllers/RegulatorCtrl.js
+++ web/app/controllers/RegulatorCtrl.js
@@ -1,5 +1,30 @@
-var regulatorCtrl = function () {
+var regulatorCtrl = function ($scope, ngDialog) {
 
+    $scope.allAssets = [{"companyID": "user1", "limit": "1000"},{"companyID": "user2", "limit": "2000"}]
+
+    $scope.openAddAssetsDlg = function () {
+        ngDialog.open({
+            template: 'addAssetsDlg',
+            className: 'ngdialog-theme-default',
+            controller: 'RegulatorCtrl'
+        });
+    };
+
+    $scope.openUpdateDlg = function () {
+        ngDialog.open({
+            template: 'updateDlg',
+            className: 'ngdialog-theme-default',
+            controller: 'RegulatorCtrl'
+        });
+    };
+
+    $scope.openDeleteDlg = function () {
+        ngDialog.open({
+            template: 'deleteDlg',
+            className: 'ngdialog-theme-default',
+            controller: 'RegulatorCtrl'
+        });
+    };
 };
 
 app.controller('RegulatorCtrl', regulatorCtrl);
